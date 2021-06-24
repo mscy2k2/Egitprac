d6d0f2a734a38d90eb8093262c4c59bb8d8760a5
Eduwem Akpan <akpane1@ferris.edu>
2021-06-24 17:52:05 -0400

    third commit for egitprac

--- spaceShipGame/EgitPractice/src/RunMain.java
+++ spaceShipGame/EgitPractice/src/RunMain.java
@@ -7,5 +7,6 @@ public class RunMain {
     public static void display() {
 	   System.out.println("To be a man, is not a day job");
 	   System.out.println("The struggle continues");
+	   System.out.println("Life is a lesson");
     }
 }
