a124220fefe5efba7a9f5c0f239eb24a2dae65d6
Eduwem Akpan <akpane1@ferris.edu>
2021-06-23 21:39:46 -0400

    First commit on EgitPractice

---
+++ spaceShipGame/EgitPractice/src/RunMain.java
@@ -0,0 +1,10 @@
+
+public class RunMain {
+
+	public static void main(String[] args) {
+		display();
+	}
+    public static void display() {
+	   System.out.println("To be a man, is not a day job");
+    }
+}
